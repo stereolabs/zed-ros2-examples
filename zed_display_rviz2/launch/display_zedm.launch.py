f4d2a12a47e1f0029009b7317e6dd5f0118f63df
Walter Lucetti <walter.lucetti@stereolabs.com>
2021-07-20 15:40:06 +0200

    Display launch files using the new configuration

--- zed_display_rviz2/launch/display_zedm.launch.py
+++ zed_display_rviz2/launch/display_zedm.launch.py
@@ -1,41 +1,32 @@
+#!/usr/bin/env python3
+
 import os
 
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    # Define LaunchDescription variable
-    ld = LaunchDescription()
 
+    # Set to False to start only Rviz2 when the ZED node is started by other launch files 
+    # For example when you run this script on a remote console to monitor the robot status.
+    start_zed_node = True
+
+    # Camera model
     # use:
     #  - 'zed' for "ZED" camera
     #  - 'zedm' for "ZED mini" camera
     #  - 'zed2' for "ZED2" camera
+    #  - 'zed2i' for "ZED2i" camera
     camera_model = 'zedm'
 
-    # Camera name
+    # Camera name. Can be different from camera model, used to distinguish camera in multi-camera systems
     camera_name = 'zedm'
-
-    # URDF file to be loaded by Robot State Publisher
-    urdf = os.path.join(
-        get_package_share_directory('zed_wrapper'),
-        'urdf', camera_model + '.urdf'
-    )
-
-    # ZED Configurations to be loaded by ZED Node
-    config_common = os.path.join(
-        get_package_share_directory('zed_wrapper'),
-        'config',
-        'common.yaml'
-    )
-
-    config_camera = os.path.join(
-        get_package_share_directory('zed_wrapper'),
-        'config',
-        camera_model + '.yaml'
-    )
 
     # Rviz2 Configurations to be loaded by ZED Node
     config_rviz2 = os.path.join(
@@ -47,29 +38,6 @@ def generate_launch_description():
     # Set LOG format
     os.environ['RCUTILS_CONSOLE_OUTPUT_FORMAT'] = '{time} [{name}] [{severity}] {message}'
 
-    # Robot State Publisher node
-    rsp_node = Node(
-        package='robot_state_publisher',
-        namespace=camera_name,
-        executable='robot_state_publisher',
-        name=camera_name+'_state_publisher',
-        output='screen',
-        arguments=[urdf],
-    )
-
-    # ZED Wrapper node
-    zed_wrapper_node = Node(
-        package='zed_wrapper',
-        namespace=camera_name,
-        executable='zed_wrapper',
-        name='zed_node',
-        output='screen',
-        parameters=[
-            config_common,  # Common parameters
-            config_camera,  # Camera related parameters
-        ]
-    )
-
     # Rviz2 node
     rviz2_node = Node(
         package='rviz2',
@@ -77,12 +45,92 @@ def generate_launch_description():
         executable='rviz2',
         name=camera_name+'_rviz2',
         output='screen',
-        arguments=[["-d"],[config_rviz2]],
+        arguments=[["-d"], [config_rviz2]],
     )
 
-    # Add noded to LaunchDescription
-    ld.add_action(rsp_node)
-    ld.add_action(zed_wrapper_node)
+    # Define LaunchDescription variable
+    ld = LaunchDescription()
+
+    if start_zed_node:
+        # Launch configuration variables (can be changed by CLI command)
+        svo_path = LaunchConfiguration('svo_path')
+
+        # Configuration variables
+        node_name = 'zed_node'  # Zed Node name
+        publish_urdf = 'true'  # Publish static frames from camera URDF
+        # Robot base frame. Note: overrides the parameter `pos_tracking.base_frame` in `common.yaml`.
+        base_frame = 'base_link'
+        # Position X of the camera with respect to the base frame [m].
+        cam_pos_x = '0.0'
+        # Position Y of the camera with respect to the base frame [m].
+        cam_pos_y = '0.0'
+        # Position Z of the camera with respect to the base frame [m].
+        cam_pos_z = '0.0'
+        # Roll orientation of the camera with respect to the base frame [rad].
+        cam_roll = '0.0'
+        # Pitch orientation of the camera with respect to the base frame [rad].
+        cam_pitch = '0.0'
+        # Yaw orientation of the camera with respect to the base frame [rad].
+        cam_yaw = '0.0'
+
+        # ZED Configurations to be loaded by ZED Node
+        config_common_path = os.path.join(
+            get_package_share_directory('zed_wrapper'),
+            'config',
+            'common.yaml'
+        )
+
+        if(camera_model != 'zed'):
+            config_camera_path = os.path.join(
+                get_package_share_directory('zed_wrapper'),
+                'config',
+                camera_model + '.yaml'
+            )
+        else:
+            config_camera_path = ''
+
+        # URDF/xacro file to be loaded by the Robot State Publisher node
+        xacro_path = os.path.join(
+            get_package_share_directory('zed_wrapper'),
+            'urdf', 'zed_descr.urdf.xacro'
+        )
+
+        # ZED Wrapper node
+        zed_wrapper_launch = IncludeLaunchDescription(
+            launch_description_source=PythonLaunchDescriptionSource([
+                get_package_share_directory('zed_wrapper'),
+                '/launch/include/zed_camera.launch.py'
+            ]),
+            launch_arguments={
+                'camera_model': camera_model,
+                'camera_name': camera_name,
+                'node_name': node_name,
+                'config_common_path': config_common_path,
+                'config_camera_path': config_camera_path,
+                'publish_urdf': publish_urdf,
+                'xacro_path': xacro_path,
+                'svo_path': svo_path,
+                'base_frame': base_frame,
+                'cam_pos_x': cam_pos_x,
+                'cam_pos_y': cam_pos_y,
+                'cam_pos_z': cam_pos_z,
+                'cam_roll': cam_roll,
+                'cam_pitch': cam_pitch,
+                'cam_yaw': cam_yaw
+            }.items()
+        )
+
+        declare_svo_path_cmd = DeclareLaunchArgument(
+            'svo_path',
+            default_value='',
+            description='Path to an input SVO file. Note: overrides the parameter `general.svo_file` in `common.yaml`.')
+
+        # Launch parameters
+        ld.add_action(declare_svo_path_cmd)
+
+        # Add nodes to LaunchDescription
+        ld.add_action(zed_wrapper_launch)
+
     ld.add_action(rviz2_node)
 
     return ld
