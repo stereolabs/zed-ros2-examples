d3209861f88bdd2ccf30c6a1b54e81d62264f5ed
Walter Lucetti <walter.lucetti@stereolabs.com>
2024-10-28 15:51:52 +0100

    Add ZED X One to rviz

--- zed_display_rviz2/launch/display_zed_cam.launch.py
+++ zed_display_rviz2/launch/display_zed_cam.launch.py
@@ -93,7 +93,7 @@ def generate_launch_description():
             DeclareLaunchArgument(
                 'camera_model',
                 description='[REQUIRED] The model of the camera. Using a wrong camera model can disable camera features.',
-                choices=['zed', 'zedm', 'zed2', 'zed2i', 'zedx', 'zedxm', 'virtual']),
+                choices=['zed', 'zedm', 'zed2', 'zed2i', 'zedx', 'zedxm', 'virtual', 'zedxonegs', 'zedxone4k']),
             OpaqueFunction(function=launch_setup)
         ]
     )
